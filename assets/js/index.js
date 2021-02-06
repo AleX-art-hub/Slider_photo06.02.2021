156eeb8377a8d67c86ae63576d051f99b167209a
BodNS <bondarenokn@gmail.com>
2021-02-06 13:53:08 +0200

    add: carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,4 +40,30 @@ class Carousel{
         return (this._curentIndex-1 + this._slides.length) % this._slides.length;
     }
 }
+ const carousel = new Carousel([
+     new Slider('https://images.ua.prom.st/1837765903_holst-dlya-risovaniya.jpg','ZhiraPH'),
+     new Slider('https://imageproxy.themaven.net//https%3A%2F%2Fwww.history.com%2F.image%2FMTYyNDg1MjE3MTI1Mjc5Mzk4%2Ftopic-london-gettyimages-760251843-promo.jpg','London'),
+     new Slider('https://thumbs.dreamstime.com/b/%D0%B2%D0%B5%D1%81%D0%B5%D0%BB%D1%8B%D0%B9-%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%B6-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%81%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D0%B0-%D0%B3%D0%B5%D1%80%D0%BC%D0%B0%D0%BD-%D1%88%D0%B5%D0%BF%D0%B0%D1%80%D0%B4-158632858.jpg','sobaka'),
+     new Slider('https://london.ac.uk/sites/default/files/styles/max_1300x1300/public/2018-10/london-aerial-cityscape-river-thames_1.jpg?itok=6LenFxuz','London2'),
+     new Slider('https://bituk.media/wp-content/uploads/2020/04/my.jpg','Milan'),
+ ]);
 
+ const [prevButton, nextButton] = document.querySelectorAll('.btn');
+
+ const sliderClick= (direction = "next")=>(event)=> {
+    carousel.currentIndex = carousel[direction =="next" ? "nextIndex": "prevIndex"];
+ };
+    prevButton.addEventListener("click", sliderClick("prev"));
+    nextButton.addEventListener("click", sliderClick("next"));
+
+    updateSlide();
+
+    function updateSlide(direction){
+    const oldCurrentImg = document.querySelector(".currentImage");
+    const newCurrentImg = document.querySelector(".nextImage");
+    const currentSlide = carousel.currentSlide;
+    //const nextSlide = carousel[direction =="next" ? "nextSlide": "prev"];
+    newCurrentImg.classList.replace("nextImage", "currentImage");
+    oldCurrentImg.classList.replace("currentImage", "nextImage");
+    newCurrentImg.setAttribute("src", currentSlide.src);
+    }
